acdf406f35825f67b0d3d008b45f0104e2ba6eba
Klaudia Krupa <eklaudiakrupa@gmail.com>
2020-06-21 21:55:09 +0200

    Main Component

---
+++ src/App/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import components from "./styles";
+import TopBar from "./TopBar";
+
+const { Main } = components;
+
+const App = () => {
+  return (
+    <Main>
+      <TopBar />
+    </Main>
+  );
+};
+
+export default App;
---
+++ src/App/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export default {
+  Main: styled.div`
+    width: 90%;
+    margin: 0 auto;
+    font-family: "Montserrat", sans-serif;
+  `,
+};

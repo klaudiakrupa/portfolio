b5c8ad0040c98fea80cbaf843b29c0285c6ae48b
Klaudia Krupa <eklaudiakrupa@gmail.com>
2020-06-29 22:11:24 +0200

    Working on contact page.

--- src/App/ContactPage/index.jsx
+++ src/App/ContactPage/index.jsx
@@ -1,27 +1,56 @@
 import React from "react";
 
+import { faMapMarkerAlt, faAt } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+
 import components from "./styles";
 
-const { Main, Heading, SmallText, Header, Contact, Icon, Text } = components;
+const {
+  Main,
+  Heading,
+  SmallText,
+  Header,
+  ContactBox,
+  Contact,
+  Text,
+  SocialMedia,
+  Icon,
+} = components;
 
 const WelcomePage = () => {
   return (
-    <Main>
+    <Main id="contact">
       <Heading>
-        <canva></canva>
         <SmallText>Zapraszam do</SmallText>
         <Header>kontaktu</Header>
       </Heading>
-      <div>
+      <ContactBox>
         <Contact>
-          <Icon></Icon>
+          <Icon icon={faAt} />
           <Text>eklaudiakrupa@gmail.com</Text>
         </Contact>
         <Contact>
-          <Icon></Icon>
+          <Icon icon={faMapMarkerAlt} />
           <Text>Rzeszów</Text>
         </Contact>
-      </div>
+      </ContactBox>
+      <SocialMedia>
+        <a
+          href="https://github.com/klaudiakrupa"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Icon icon={faGithub} link />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/klaudiakrupa/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Icon icon={faLinkedinIn} link />
+        </a>
+      </SocialMedia>
     </Main>
   );
 };
--- src/App/ContactPage/styles.js
+++ src/App/ContactPage/styles.js
@@ -1,13 +1,16 @@
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default {
   Main: styled.div`
     width: 100%;
+    height: 100%;
     color: #fff;
     background-color: #1a1a1a;
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
   `,
   Heading: styled.div``,
   SmallText: styled.div`
@@ -18,7 +21,41 @@ export default {
     font-size: 5rem;
     font-weight: 700;
   `,
-  Contact: styled.div``,
-  Icon: styled.div``,
+
+  ContactBox: styled.div`
+    font-size: 1.4rem;
+    padding: 4rem;
+    width: 45%;
+    min-width: 400px;
+    flex-wrap: wrap;
+    display: flex;
+    justify-content: space-evenly;
+  `,
+
+  Contact: styled.div`
+    display: flex;
+  `,
   Text: styled.div``,
+
+  SocialMedia: styled.div``,
+
+  Icon: styled(FontAwesomeIcon)`
+    font-size: 1.8rem;
+    color: #fff;
+    padding: 0 0.7rem;
+
+    ${(props) =>
+      props.link &&
+      `
+      font-size: 2rem;
+    cursor: pointer;
+    transition: all 0.3s;
+    opacity: 0.3;
+    
+    &:hover {
+      opacity: 0.8;
+      color: #FECE2F;
+    }
+    `}
+  `,
 };

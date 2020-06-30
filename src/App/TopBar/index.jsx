151be20d4aa2543a050f898160907c8ad3f21efe
Klaudia Krupa <eklaudiakrupa@gmail.com>
2020-06-30 14:19:07 +0200

    Scrolling to sections from top bar menu

--- src/App/TopBar/index.jsx
+++ src/App/TopBar/index.jsx
@@ -4,15 +4,21 @@ import components from "./styles";
 const { TopBar, LogoBox, Logo, Menu, MenuItem } = components;
 
 const TopBarComponent = () => {
+  const scrollTo = (id) => {
+    const element = document.getElementById(id);
+    if (element)
+      element.scrollIntoView({ block: "center", behavior: "smooth" });
+  };
+
   return (
     <TopBar>
       <LogoBox>
         <Logo src="/logo_eklaudiakrupa.png" />
       </LogoBox>
       <Menu>
-        <MenuItem>o mnie</MenuItem>
-        <MenuItem>portfolio</MenuItem>
-        <MenuItem>umiejętności</MenuItem>
+        <MenuItem onClick={() => scrollTo("aboutMe")}>o mnie</MenuItem>
+        <MenuItem onClick={() => scrollTo("portfolio")}>portfolio</MenuItem>
+        <MenuItem onClick={() => scrollTo("skills")}>umiejętności</MenuItem>
         <MenuItem
           href="/CV_Klaudia_Krupa.pdf"
           target="_blank"
@@ -20,7 +26,7 @@ const TopBarComponent = () => {
         >
           pobierz CV
         </MenuItem>
-        <MenuItem>kontakt</MenuItem>
+        <MenuItem onClick={() => scrollTo("contact")}>kontakt</MenuItem>
       </Menu>
     </TopBar>
   );
